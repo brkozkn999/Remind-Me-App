29435ce51ab2e8b997140c47d81734e98f81ae3c
brkozkn999 <ibrahimberkozkan34@gmail.com>
2024-05-21 01:59:39 +0300

    upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Separator } from "@/components/ui/separator";
+import NavBar from "@/components/NavBar";
+import { ThemeProvider } from "@/providers/ThemeProviders";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +20,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={cn(inter.className, 'dark')} style={{colorScheme: 'dark'}}>
+        <body>
+          <ThemeProvider>
+            <div className="flex min-h-screen w-full flex-col items-center dark:bg-black">
+              <NavBar />
+              <Separator/>
+              <main className="flex flex-grow w-full justify-center items-center dark:bg-neutral-950">
+                {children}
+              </main>
+            </div>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <h1 className="text-2xl font-bold bg-gradient-to-r from-pink-500 to-yellow-500 bg-clip-text text-transparent">
+        RemindMe
+    </h1>
+  );
+}
+
+export default Logo;
